95b24ad107beda85a27d10575f7e1a09e06d9222
Michał Zamorski <zamorski.michal@gmail.com>
2018-03-18 21:59:39 +0100

    import changes

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import {StyleSheet, View } from 'react-native';
 import {NavigationScreenProp} from 'react-navigation';
 import {Icon, Text} from 'react-native-elements';
