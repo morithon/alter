00814bb08c71100808eecd62ed87f2183442756f
Michał Zamorski <zamorski.michal@gmail.com>
2018-03-23 21:27:37 +0100

    resolved conflict

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,56 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import {StyleSheet, View } from 'react-native';
-import {NavigationScreenProp} from 'react-navigation';
-import {Icon, Text} from 'react-native-elements';
-import {lightGray, lighterGray, brightBlue, darkBrightBlue, orange} from '../styles/colors';
-import { utils } from '../styles/utils';
-
-export interface HomeScreenProps {
-	navigation: NavigationScreenProp<{}>;
-};
-
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'white',
-		justifyContent: 'center',
-	},
-	row: {
-		flex: 5,
-		flexDirection: 'row',
-		alignItems: 'center'
-	},
-	column: {
-		flex: 1,
-		alignItems: 'center',
-	},
-	disabled: {
-		color: lightGray
-	},
-	header: {
-		flex: 5,
-		backgroundColor: orange,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	headerText: {
-		color: 'white',
-		fontSize: 80,
-		fontWeight: 'bold',
-	},
-	topSpace: {
-		flex: 1
-	},
-	bottomSpace: {
-		flex: 1
-	}
-});
-
-export default class HomeScreen extends React.Component<HomeScreenProps, {}> {
-	render() {
-=======
 import {View} from 'react-native';
 import {Icon, Text} from 'react-native-elements';
 import {NavigationScreenProps} from 'react-navigation';
@@ -62,7 +10,6 @@ import {utils} from '../styles/utils';
 
 export default class HomeScreen extends React.Component<NavigationScreenProps, {}> {
 	public render() {
->>>>>>> Stashed changes
 		const {navigate} = this.props.navigation;
 		return (
 			<View style={styles.container}>
@@ -90,57 +37,6 @@ export default class HomeScreen extends React.Component<NavigationScreenProps, {
 							Calm
 						</Text>
 					</View>
-<<<<<<< Updated upstream
-					<View style={styles.column} >
-						<Icon
-							name="glasses"
-							type="material-community"
-							color={lighterGray}
-							reverse={true}
-							reverseColor={lightGray}
-							size={32}
-							raised={false}
-						/>
-						<Text style={styles.disabled}>
-							Sharp
-						</Text>
-						<Text style={styles.disabled}>
-							(Coming Fall 2018)
-						</Text>
-					</View>
-				</View>
-				<View style={styles.row}> 
-					<View style={styles.column} >
-						<Icon
-							name="block"
-							color={lighterGray}
-							reverse={true}
-							reverseColor={lightGray}
-							size={32}
-						/>
-						<Text style={styles.disabled}>
-							Sober
-						</Text>
-						<Text style={styles.disabled}>
-							(TBD)
-						</Text>
-					</View>
-					<View style={styles.column} >
-						<Icon
-							name="block"
-							color={lighterGray}
-							reverse={true}
-							reverseColor={lightGray}
-							size={32}
-						/>
-						<Text style={styles.disabled}>
-							Non-smoking
-						</Text>
-						<Text style={styles.disabled}>
-							(TBD)
-						</Text>
-					</View>
-=======
 					<DisabledIconButton
 						iconName="glasses"
 						iconType="material-community"
@@ -155,15 +51,10 @@ export default class HomeScreen extends React.Component<NavigationScreenProps, {
 					<DisabledIconButton
 						caption="Non-smoking"
 					/>
->>>>>>> Stashed changes
 				</View>
 				<View style={styles.bottomSpace}>
 				</View>
 			</View>
 		);
 	}
-<<<<<<< Updated upstream
 }
-=======
-}
->>>>>>> Stashed changes
