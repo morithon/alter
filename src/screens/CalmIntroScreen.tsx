36fb6c14c2c709b8060d8102c1163cf90cf6592e
Michał Zamorski <zamorski.michal@gmail.com>
2018-04-21 13:05:03 +0200

    Intro text formatting to work better on all screen sizes

--- src/screens/CalmIntroScreen.tsx
+++ src/screens/CalmIntroScreen.tsx
@@ -7,25 +7,25 @@ const slides: Slide[] = [
 	{
 		key: 'anxiety',
 		title: 'Anxious?',
-		text: 'The world is full of bad things and it\'s\n important to be aware of them.\n' +
-			'But sometimes we can focus on them\n too much. This game tries to help\n' +
-			'with that. Here\'s how it works:',
+		text: 'The world is full of bad things and it\'s important to be aware of them. ' +
+			'But sometimes we can focus on them too much. This game tries to help ' +
+			'with that.\n\nHere\'s how it works:',
 		backgroundColor: '#169cf9'
 	},
 	{
 		key: 'attention',
 		title: 'What do I do?',
-		text: 'Two words appear on the screen for a\nsplit second: one negative and\n' +
-		'one neutral. When they disappear, you\nwill see a marker where the neutral word\n' +
-		'used to be. When you press it,\nnew words will appear.',
+		text: 'Two words appear on the screen for a split second: one negative and ' +
+		'one neutral. When they disappear, you will see a marker where the neutral word ' +
+		'used to be. When you press it, new words will appear.',
 		backgroundColor: '#ea7317'
 	},
 	{
 		key: 'why',
 		title: 'Ok, but why?',
-		text: 'The goal is to train your instinctive\nresponse to focus away from the\nnegative. ' +
-		'If you learn how to pay less\nattention to the negative,\nyou might be less anxious.\n\n' +
-		'So go ahead, try it:',
+		text: 'The goal is to train your instinctive response to focus away from the negative. ' +
+		'If you learn how to pay less attention to the negative, you might be less anxious.\n\n' +
+		'So go ahead, try it!',
 		backgroundColor: '#2364aa'
 	},
 ];
