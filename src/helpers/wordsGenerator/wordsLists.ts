0314334de4e868dad7da57bb021335172442ee1c
Michał Zamorski <zamorski.michal@gmail.com>
2018-06-23 20:26:56 +0200

    typing hack

--- src/helpers/wordsGenerator/wordsLists.ts
+++ src/helpers/wordsGenerator/wordsLists.ts
@@ -1,10 +1,13 @@
 import I18n from '../../i18n/i18n';
 import WordsLists from './interfaces/WordsLists';
 
+// I18n.t can actually return string[], just not typed well
+const translate = I18n.t as (scope: string) => string | string[];
+
 const wordsLists: WordsLists = {
-	positive: I18n.t('positiveWords') as any, // I18n can actually return string[], just not typed well
-	neutral: I18n.t('neutralWords') as any,
-	negative: I18n.t('negativeWords') as any
+	positive: translate('positiveWords') as string[],
+	neutral: translate('neutralWords') as string[],
+	negative: translate('negativeWords') as string[]
 };
 
 export default wordsLists;
